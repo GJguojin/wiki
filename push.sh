7742f39e35e39115c1db9fe90ae3cc138a93a848
jin.guo <1509667407@qq.com>
2021-02-20 22:16:41 +0800

    modify

--- push.sh
+++ push.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+echo "git add ."
 git add .
+
+echo ""
+echo "**************************"
+echo "git commit"
 git commit -m "modify"
 
+
+echo ""
+echo "**************************"
+echo "git pull"
 git pull
 
+
+echo ""
+echo "**************************"
+echo "git push"
 git push
 
 sleep 2s
 
+echo ""
+echo "**************************"
+echo " ssh git pill"
 ssh root@120.48.26.59  /opt/wiki/command.sh
